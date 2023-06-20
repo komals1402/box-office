3b2a13395b883cfc2a587a1bb21be5381d11911d
komals1402 <ks387406@gmail.com>
2023-06-20 21:50:38 +0530

    fetch and display data from tvmaze api. create reusable function to fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-/* eslint-disable no-restricted-globals */
 import { useState } from "react";
-
+import { searchForShows } from "./../api/tvmaze";
 const Home = () => {
   const [searchStr, setSearchStr] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
+  console.log(apiDataError);
   const onSearchInputChange = (ev) => {
     setSearchStr(ev.target.value);
   };
@@ -11,11 +13,26 @@ const Home = () => {
   const onSearch = async (ev) => {
     ev.preventDefault();
 
-    const response = await fetch("https://api.tvmaze.com/search/shows?q=boys");
-    const body = await response.json();
-    console.log(body);
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
 
-    //https://api.tvmaze.com/search/shows?q=boys
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occured: {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
   };
 
   return (
@@ -24,6 +41,8 @@ const Home = () => {
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
         <button type="submit">Search</button>
       </form>
+
+      <div>{renderApiData()}</div>
     </div>
   );
 };
