9c6d752d2a3c6041749581bc13081758564e969f
komals1402 <ks387406@gmail.com>
2023-06-23 12:19:48 +0530

    used useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,22 @@ import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query';
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
-  const onSearch = async ({ q, searchOption }) => {
-    try {
-      setApiDataError(null);
-      let result;
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
-  };
+  const [filter, setFilter] = useState(null);
 
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      fetch.searchOption === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
+  const onSearch = async ({ q, searchOption }) => {
+    setFilter({ q, searchOption });
+  };
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Error occured: {apiDataError.message}</div>;
