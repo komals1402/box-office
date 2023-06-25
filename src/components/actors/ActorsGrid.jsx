8bbd2d21e48a8d622bb86076b925746d01236a65
komals1402 <ks387406@gmail.com>
2023-06-25 22:03:20 +0530

    move not found imageto src and then import it instead of serving from public folder

--- src/components/actors/ActorsGrid.jsx
+++ src/components/actors/ActorsGrid.jsx
@@ -1,5 +1,7 @@
 import { FlexGrid } from '../common/FlexGrid';
 import ActorCard from './ActorCard';
+import NotFoundImgSrc from '../../lib/not-found-image.png';
+
 const ActorsGrid = ({ actors }) => {
   return (
     <FlexGrid>
@@ -11,11 +13,7 @@ const ActorsGrid = ({ actors }) => {
           birthday={data.person.birthday}
           deathday={data.person.deathday}
           gender={data.person.gender}
-          image={
-            data.person.image
-              ? data.person.image.medium
-              : '/not-found-image.png'
-          }
+          image={data.person.image ? data.person.image.medium : NotFoundImgSrc}
         />
       ))}
     </FlexGrid>
